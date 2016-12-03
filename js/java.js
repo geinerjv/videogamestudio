780e9e7d0f4f43b3f7bbb06753b69acd30264659
Geiner Jiménez Villegas <linkmasck@gmail.com>
2016-12-03 14:16:40 -0600

    Se adjunta codigo jQuery para el menu

--- js/java.js
+++ js/java.js
@@ -1,1 +1,14 @@
-alert("sddd");
+$(document).ready(function(){
+  $( "#btn-menu" ).click(function() {
+      $("#btn-menu").hide();  
+      $("#btn-menu-esconder").show();
+      $("#nav").slideDown();
+  });
+    
+    $("#btn-menu-esconder").click(function(){
+        $("#btn-menu").show(); 
+        $("#btn-menu-esconder").hide();
+        $("#nav").slideUp();
+    });
+}); 
+
